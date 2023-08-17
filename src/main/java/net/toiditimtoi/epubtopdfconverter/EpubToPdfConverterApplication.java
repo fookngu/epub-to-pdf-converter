946a8fe3214cd0743513e61008509e21118aaf81
kukot <phucnn.work@gmail.com>
2023-08-17 23:14:08 +0800

    Initial commit with barebone Spring web project

---
+++ src/main/java/net/toiditimtoi/epubtopdfconverter/EpubToPdfConverterApplication.java
@@ -0,0 +1,13 @@
+package net.toiditimtoi.epubtopdfconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpubToPdfConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpubToPdfConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/toiditimtoi/epubtopdfconverter/EpubToPdfConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package net.toiditimtoi.epubtopdfconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpubToPdfConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
